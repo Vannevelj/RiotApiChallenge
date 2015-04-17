9f5a54a6e2739738ecaecc6ce0b69b9c3372916d
Vannevelj <jer_vannevel@hotmail.com>
2015-04-17 18:39:53 +0200

    ImageDownloader now also downloads summoner spells and saves the images with the ID instead

--- RiotApiChallenge/ImageDownloader/Program.cs
+++ RiotApiChallenge/ImageDownloader/Program.cs
@@ -18,6 +18,7 @@ namespace ImageDownloader
         {
             AsyncContext.Run(() => new Program().MainAsync(args));
             Console.WriteLine("All done!");
+            Console.Read();
         }
 
         public async Task MainAsync(string[] args)
@@ -29,15 +30,37 @@ namespace ImageDownloader
             {
                 foreach (var version in versions.OrderByDescending(x => x))
                 {
-                    var image = await GetImageAsync(version, champion);
+                    var url = string.Format("http://ddragon.leagueoflegends.com/cdn/{0}/img/champion/{1}.png", version, champion.Item1);
+                    var image = await GetImageAsync(url);
                     if (image != null)
                     {
                         using (var bitmap = new Bitmap(image))
                         {
-                            var location = string.Format("C:\\Temp\\champion_{0}.png", champion.ToLower());
+                            var location = string.Format("C:\\Temp\\champion_{0}.png", champion.Item2);
                             bitmap.Save(location, ImageFormat.Png);
 
                             Console.WriteLine("Saved {0} to {1}", champion, location);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var summonerSpells = (await GetSummonerSpellsAsync()).ToArray();
+            foreach (var summonerSpell in summonerSpells)
+            {
+                foreach (var version in versions.OrderByDescending(x => x))
+                {
+                    var url = string.Format("http://ddragon.leagueoflegends.com/cdn/{0}/img/spell/{1}.png", version, summonerSpell.Item1);
+                    var image = await GetImageAsync(url);
+                    if (image != null)
+                    {
+                        using (var bitmap = new Bitmap(image))
+                        {
+                            var location = string.Format("C:\\Temp\\summonerspell_{0}.png", summonerSpell.Item2);
+                            bitmap.Save(location, ImageFormat.Png);
+
+                            Console.WriteLine("Saved {0} to {1}", summonerSpell, location);
                             break;
                         }
                     }
@@ -55,20 +78,30 @@ namespace ImageDownloader
             }
         }
 
-        public async Task<IEnumerable<string>> GetChampionsAsync()
+        public async Task<IEnumerable<Tuple<string, string>>> GetChampionsAsync()
         {
             const string url = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key=d20a2910-08fd-43c7-8ef1-e6308b938c8b";
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(url);
                 var content = JsonConvert.DeserializeObject<JObject>(await response.Content.ReadAsStringAsync());
-                return content["data"].Value<JObject>().Properties().Select(x => x.Name).ToArray();
+                return content["data"].Value<JObject>().Properties().Select(x => new Tuple<string, string>(x.Name, x.Value.Value<string>("id"))).ToArray();
             }
         }
 
-        public async Task<Stream> GetImageAsync(string version, string champion)
+        public async Task<IEnumerable<Tuple<string, string>>> GetSummonerSpellsAsync()
         {
-            var url = string.Format("http://ddragon.leagueoflegends.com/cdn/{0}/img/champion/{1}.png", version, champion);
+            const string url = "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/summoner-spell?api_key=d20a2910-08fd-43c7-8ef1-e6308b938c8b";
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var content = JsonConvert.DeserializeObject<JObject>(await response.Content.ReadAsStringAsync());
+                return content["data"].Value<JObject>().Properties().Select(x => new Tuple<string, string>(x.Name, x.Value.Value<string>("id"))).ToArray();
+            }
+        }
+
+        public async Task<Stream> GetImageAsync(string url)
+        {
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(url);
